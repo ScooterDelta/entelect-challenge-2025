db039b128b2b37478af902f9fb39b1e120bcfc3b
Anthony Farquharson <afarquharson@munichre.com>
2025-07-10 18:00:52 +0100

    Adds score

--- node/src/index.ts
+++ node/src/index.ts
@@ -2,6 +2,7 @@ import { getAllowedResources } from "./get-resources";
 import { fillGridDump } from "./grid";
 import { readInputFile } from "./input-reader";
 import { readResourceFile } from "./resource-reader";
+import { calculateScore } from "./score";
 import { ResourcesData } from "./types/resources";
 
 const resources: ResourcesData = readResourceFile();
@@ -9,9 +10,8 @@ const input1 = await readInputFile(1);
 
 console.log("Resources Data Loaded Successfully");
 
-const allowedReaources = getAllowedResources(resources,input1.available_resources);
-
+const allowedReaources = getAllowedResources(resources, input1.available_resources);
 
 const resultGrid = fillGridDump(input1.grid, allowedReaources);
 
-console.log(resultGrid);
+console.log(calculateScore(resultGrid));
