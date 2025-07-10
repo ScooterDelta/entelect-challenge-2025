8b1b0cac93b749108d37b02215f853d088a47a75
tyrone <tyroneallen281@live.co.uk>
2025-07-10 17:43:56 +0100

    Test

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,3 +1,5 @@
+import { getAllowedResources } from "./get-resources";
+import { fillGridDump } from "./grid";
 import { readInputFile } from "./input-reader";
 import { readResourceFile } from "./resource-reader";
 import { ResourcesData } from "./types/resources";
@@ -6,3 +8,10 @@ const resources: ResourcesData = readResourceFile();
 const input1 = await readInputFile(1);
 
 console.log("Resources Data Loaded Successfully");
+
+const allowedReaources = getAllowedResources(resources,input1.available_resources);
+
+
+const resultGrid = fillGridDump(input1.grid, allowedReaources);
+
+console.log(resultGrid);
