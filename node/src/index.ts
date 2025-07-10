be86562f0b4df5b749b5790f05baeecf2f2296ee
Anthony Farquharson <afarquharson@munichre.com>
2025-07-10 17:18:36 +0100

    Fixes ts issues

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,5 +1,12 @@
+import { readFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { readInputFile } from "./input-reader";
 import { ResourcesData } from "./types/resources";
 
-const resources: ResourcesData = require("../input/resources.json");
+const currentDirectory = dirname(fileURLToPath(import.meta.url));
+
+const resources: ResourcesData = JSON.parse(readFileSync(join(currentDirectory, "..", "input", "resources.json"), "utf-8"));
+const input1 = await readInputFile(1);
 
 console.log("Resources Data Loaded Successfully");
