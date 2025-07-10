1a713cac88e93fce2d7a806fb0f39085dcbf436d
Anthony Farquharson <afarquharson@munichre.com>
2025-07-10 17:26:18 +0100

    Adds file read

--- node/src/input-reader.ts
+++ node/src/input-reader.ts
@@ -5,8 +5,8 @@ import { Challenge } from './types/grid';
 
 const currentDirectory = dirname(fileURLToPath(import.meta.url));
 
-const zooSize = /^Zoo Size:.+(\d+)x(\d+)$/g;
-const resources = /^Available Resources:.+\[(\d+)+\]$/g;
+const zooSize = /Zoo Size:.(\d+)x(\d+)/g;
+const resources = /Available Resources:.+\[(.+)]/g;
 
 const createGrid = (rows: number, cols: number): number[][] => {
   const grid: number[][] = [];
@@ -24,18 +24,18 @@ export const readInputFile = async (challengeId: number): Promise<Challenge> => 
   const challengePath = join(currentDirectory, '..', 'input', `${challengeId}.txt`);
   const file = await fs.readFile(challengePath, 'utf-8');
 
-  const gridSize = file.match(zooSize);
-  const resourcesNumbers = file.match(resources);
+  const gridSize = [...file.matchAll(zooSize)][0];
+  const resourcesNumbers = [...file.matchAll(resources)][0];
 
   if (!resourcesNumbers)
     throw new Error(`Available resources not found in challenge file: ${challengePath}`);
   if (!gridSize)
     throw new Error(`Zoo size not found in challenge file: ${challengePath}`);
 
-  const parsedGridSize = [parseInt(gridSize[0]), parseInt(gridSize[1])];
+  const parsedGridSize = [parseInt(gridSize[1]), parseInt(gridSize[2])];
   return {
     grid: createGrid(parsedGridSize[0], parsedGridSize[1]),
-    available_resources: resourcesNumbers.map((r) => parseInt(r)),
+    available_resources: resourcesNumbers[1].split(', ').map((r) => parseInt(r)),
     zoo_size: parsedGridSize,
     challenge_id: challengeId
   }
