2ea0c92b64d2d31af1b3500a89d2191fae9c61b6
Anthony Farquharson <afarquharson@munichre.com>
2025-07-10 17:28:55 +0100

    Adds resource reader

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,12 +1,8 @@
-import { readFileSync } from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
 import { readInputFile } from "./input-reader";
+import { readResourceFile } from "./resource-reader";
 import { ResourcesData } from "./types/resources";
 
-const currentDirectory = dirname(fileURLToPath(import.meta.url));
-
-const resources: ResourcesData = JSON.parse(readFileSync(join(currentDirectory, "..", "input", "resources.json"), "utf-8"));
+const resources: ResourcesData = readResourceFile();
 const input1 = await readInputFile(1);
 
 console.log("Resources Data Loaded Successfully");
